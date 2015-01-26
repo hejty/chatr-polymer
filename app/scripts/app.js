32126c3b019c25b93d03ca90e62931bed1175588
Lubos Belak <belak.lubos@gmail.com>
2015-01-26 19:10:14 +0100

    Wrapping the entire app into a single element

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,11 +2,7 @@
   'use strict';
 
   document.addEventListener('polymer-ready', function() {
-    var hamburger = document.getElementById('hamburger');
-    var channelPanel = document.getElementById('channel-panel');
-    hamburger.addEventListener('click', function() {
-        channelPanel.togglePanel();
-    })
+
   });
 
 // wrap document so it plays nice with other libraries
